914670df90a1eddc51b154d1f7aa1d2c16b76b47
LPedro20020 <pilopezvargas@knox.edu>
2024-11-06 04:06:32 -0600

    feat: add thank you section and remove unused script

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('yesNoCheckbox').addEventListener('change', function() {
-    var additionalInputs = document.getElementById('additionalInputs');
-    if (this.checked) {
-        additionalInputs.style.display = 'block';
-    } else {
-        additionalInputs.style.display = 'none';
-    }
-});
